237c7a0ad7fa423e5b9dd0d4f9c8bf84a1cdfdb3
Anderson Costa <costaandersom23@gmail.com>
2025-03-21 19:07:23 -0300

    feat cv dowloader

--- components/section/NavBar/index.tsx
+++ components/section/NavBar/index.tsx
@@ -10,15 +10,67 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 function NavBar() {
   return (
-    <nav className="fixed top-0 right-0 px-5 py-4 flex lg:gap-20 gap-5 text-xl text-background">
+    <nav className="fixed top-0 right-0 px-5 py-4 flex lg:gap-20 gap-5 text-xl text-background z-10">
       <ul className="hidden lg:flex gap-5 bg-white lg:px-5 lg:py-2 px-1 py-1 rounded-full justify-center items-center shadow-lg shadow-gray-950">
-        <li>Home</li>
-        <li>Projetos</li>
-        <li>Contato</li>
-        <li className="bg-background text-white lg:px-5 lg:py-2 px-2 py-1 rounded-full">
-          CV
+        <li>
+          <Link href={"#"} className="hover:text-amber-500 transition">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="hover:text-amber-500 transition">
+            Sobre mim
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="hover:text-amber-500 transition">
+            Serviços
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="hover:text-amber-500 transition">
+            Projetos
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="hover:text-amber-500 transition">
+            Contatos
+          </Link>
+        </li>
+        <li className="bg-background text-white lg:px-5 lg:py-2 px-2 py-1 rounded-full hover:bg-amber-500 transition-colors">
+          <AlertDialog>
+            <AlertDialogTrigger>CV</AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Deseja baixar o CV?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Ao clicar voce ira baixar o Curriculo virtual
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>
+                  <a href="/cv.docx" download>
+                    Confirma
+                  </a>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </li>
       </ul>
       <Sheet>
